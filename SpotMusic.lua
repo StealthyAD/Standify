af99637f7dba2d1d6f877ba8e050eff9fc9a8973
StealthyAD <125747450+StealthyAD@users.noreply.github.com>
2023-02-18 20:05:49 +0100

    Update SpotMusic.lua

--- SpotMusic.lua
+++ SpotMusic.lua
@@ -9,10 +9,10 @@
 -------------------
 
 local aalib = require("aalib")
-local PlaySound = aalib.play_sound
+local SpotPlaySound = aalib.play_sound
 local SND_ASYNC<const> = 0x0001
 local SND_FILENAME<const> = 0x00020000
-local version = "0.12"
+local version = "0.13"
 
 util.require_natives(1663599433)
 util.keep_running()
@@ -77,19 +77,31 @@ end
             local _, filename, ext = string.match(filepath, "(.-)([^\\/]-%.?([^%.\\/]*))$")
             if not filesystem.is_dir(script_store_dir) and ext == "wav" then
                 local name = string.gsub(filename, "%.wav$", "")
+                local sound_location = join_path(script_store_dir, name .. ".wav")
                 SpotLoadedSongs[#SpotLoadedSongs + 1] = {file=name, sound=aalib.play_sound(sound_location, SND_FILENAME | SND_ASYNC)}
             end
         end
         return SpotLoadedSongs
     end
 
+    function GetFileNameFromPath(path) -- Redirect to the filename
+        if path and path ~= "" then -- check if path is not nil or empty
+            return path:match("^.+/(.+)$")
+        end
+        return nil
+    end
+
 --------------------------------
 -- Main Menu Features
 --------------------------------
 
-    SpotRoot:action("Restart Script", {'spotrestart'}, "", function()
+    local sound_handle = nil
+
+    SpotRoot:action("Restart Script", {'spotrestart'}, "Restart instantly the script if have any problems.\nNOTE: It will Instantly shut down music.", function()
+        sound_handle = aalib.play_sound(join_path(script_store_dir_stop, "stop.wav"), SND_FILENAME | SND_ASYNC)
         util.restart_script()
     end)
+
     SpotRoot:divider("Main Menu")
     SpotRoot:hyperlink("Open Music Folders", "file://"..script_store_dir, "Edit your music and enjoy.\nNOTE: You need to put .wav file.\nMP3 or another files contains invalid file are not accepted.")
 
@@ -97,17 +109,14 @@ end
     -- Stop Sounds
     --------------------------------
 
-    local sound_handle = nil
-
-    SpotRoot:action("Stop Music", {'spotstopmusic'}, "It will stop your music instantly.\nNOTE: Don't delete the folder called Stop Sounds, music won't stop and looped. Don't rename file.", function()
-        local sound_location = join_path(script_store_dir_stop, "stop.wav")
-        if not filesystem.exists(sound_location) then
-            util.toast("[SpotMusic] : Sound file does not exist: " .. sound_location)
+    SpotRoot:action("Stop Music", {'spotstopmusic'}, "It will stop your music instantly.\nNOTE: Don't delete the folder called Stop Sounds, music won't stop and looped. Don't rename file.", function(selected_index)
+        local sound_location_1 = join_path(script_store_dir_stop, "stop.wav")
+        if not filesystem.exists(sound_location_1) then
+            util.toast("> SpotMusic : Sound file does not exist: " .. sound_location_1.."\n\nNOTE: You need to get the file, otherwise you can't stop the sound.")
         else
-        sound_handle = aalib.play_sound(sound_location, SND_FILENAME | SND_ASYNC)
-        if sound_handle ~= nil then
-            aalib.stop_sound(sound_handle)
-            sound_handle = nil
+            sound_handle = aalib.play_sound(sound_location_1, SND_FILENAME | SND_ASYNC)
+            if SpotFiles and SpotFiles ~= "" then -- check if SpotFiles is not nil or empty
+                util.toast('> SpotMusic\nMusic stopped.')
             end
         end
     end)
@@ -123,11 +132,11 @@ end
         local selected_file = SpotFiles[selected_index]
         local sound_location = join_path(script_store_dir, selected_file)
         if not filesystem.exists(sound_location) then
-            util.toast("Sound file does not exist: " .. sound_location)
+            util.toast("> SpotMusic : Sound file does not exist: " .. sound_location)
         else
-            local display_text = string.gsub(selected_file, "%.wav$", "") -- remove last 4 characters (".wav")
-            PlaySound(sound_location, SND_FILENAME | SND_ASYNC)
-            util.toast("SpotMusic Script\nMusic choosen: " .. display_text)
+            local display_text = string.gsub(string.gsub(selected_file, "%.wav$", ""), "%.WAV$", "")
+            SpotPlaySound(sound_location, SND_FILENAME | SND_ASYNC)
+            util.toast("> SpotMusic\nMusic choosen: " .. display_text)
         end
     end)
 
@@ -147,19 +156,35 @@ end
         util.toast("Hello ".. players.get_name(players.user()).. "\nWelcome to SpotMusic " ..version)
     end
 
+    if SCRIPT_SILENT_STOP then
+        util.toast("> SpotMusic\n\nThank you for using the script and enjoy for the last feature v"..version)
+    end
+
     --------------------------------
     -- Credits & GitHub
     --------------------------------
 
-    local SpotCreditsAndMiscs = SpotRoot:list("Miscs")
-    SpotCreditsAndMiscs:divider("Informations")
-    SpotCreditsAndMiscs:action("Version: " ..version, {}, "", function()end)
-    SpotCreditsAndMiscs:hyperlink("Github Link", "https://github.com/StealthyAD/SpotMusic")
+    local SpotMiscs = SpotRoot:list("Miscellaneous")
+    ----------------
+    -- Informations
+    ----------------
 
-    SpotCreditsAndMiscs:divider("Credits")
-    SpotCreditsAndMiscs:action("StealthyAD.#8293 (Developer SpotMusic)", {}, "", function()end)
-    SpotCreditsAndMiscs:action("Lance", {}, "Created Startup Sound and I improve the lua to create Playlists and make greater.", function()end)
+        SpotMiscs:divider("Informations")
+        SpotMiscs:action("Version: " ..version, {}, "", function()end)
+        SpotMiscs:hyperlink("Github Link", "https://github.com/StealthyAD/SpotMusic")
 
-    SpotCreditsAndMiscs:divider("Resources")
-    SpotCreditsAndMiscs:hyperlink("Stand API", "https://stand.gg/help/lua-api-documentation", "Provides much features & essentials for Lua Scripts.")
-    SpotCreditsAndMiscs:hyperlink("NativeDB", "https://nativedb.dotindustries.dev/natives", "Provided for natives.")
+    -------------
+    -- Credits
+    -------------
+
+        SpotMiscs:divider("Credits")
+        SpotMiscs:action("StealthyAD.#8293 (Developer SpotMusic)", {}, "", function()end)
+        SpotMiscs:action("Lance", {}, "Created Startup Sound and I improve the lua to create Playlists and make easier.", function()end)
+    
+    -------------
+    -- Resources
+    -------------
+
+        SpotMiscs:divider("Resources")
+        SpotMiscs:hyperlink("Stand API", "https://stand.gg/help/lua-api-documentation", "Provides much features & essentials for Lua Scripts.")
+        SpotMiscs:hyperlink("NativeDB", "https://nativedb.dotindustries.dev/natives", "Provided for natives.")
