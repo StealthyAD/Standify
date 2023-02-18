6e70b6f787a9a99721035acd2271e079f0ad3da5
StealthyAD <125747450+StealthyAD@users.noreply.github.com>
2023-02-19 00:14:08 +0100

    Update SpotMusic.lua

--- SpotMusic.lua
+++ SpotMusic.lua
@@ -123,13 +123,6 @@ end
         verify_file_begins_with="--"
     })
 
-    -- Run Manual Update
-    menu.action(SpotRoot, "Check for Update", {}, "The script will automatically check for updates at most daily, but you can manually check using this option anytime.", function()
-        auto_update_config.check_interval = 0
-        util.toast("Checking for updates")
-        auto_updater.run_auto_update(auto_update_config)
-    end)
-
 --------------------------------
 -- Translations Features
 --------------------------------
